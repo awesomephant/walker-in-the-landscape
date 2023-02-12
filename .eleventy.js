ee52da60c02d851c6d8f0c59b771b81739989027
Max Kohler <hello@maxkohler.com>
2023-02-12 19:14:29 +0100

    Clean out dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,35 +1,31 @@
-const pluginSass = require("eleventy-plugin-sass");
-const markdownIt = require("markdown-it");
+const markdownIt = require("markdown-it")
 
 module.exports = function (eleventyConfig) {
+  let options = {
+    html: true,
+    breaks: false,
+    linkify: true,
+  }
 
-    let options = {
-        html: true,
-        breaks: false,
-        linkify: true
-    };
+  eleventyConfig.addPairedShortcode("header", function (content) {
+    return `<header class='section-header'>${content}</header>`
+  })
+  eleventyConfig.addPairedShortcode("footer", function (content) {
+    return `<footer class='play-footer'>${content}</footer>`
+  })
 
+  eleventyConfig.addPairedShortcode("image", function (content, url) {
+    return `<p data-image="${url}">${content}</p>`
+  })
 
-    eleventyConfig.addPairedShortcode("header", function (content) {
-        return `<header class='section-header'>${content}</header>`
-    });
-    eleventyConfig.addPairedShortcode("footer", function (content) {
-        return `<footer class='play-footer'>${content}</footer>`
-    });
+  eleventyConfig.addPassthroughCopy("js")
+  eleventyConfig.addPassthroughCopy("assets")
+  eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy("/*.png")
+  eleventyConfig.addPassthroughCopy("/*.png")
+  eleventyConfig.addPassthroughCopy("/*.xml")
+  eleventyConfig.addPassthroughCopy("favicon.ico")
+  eleventyConfig.addPassthroughCopy("site.webmanifest")
 
-    eleventyConfig.addPairedShortcode("image", function (content, url) {
-        return `<p data-image="${url}">${content}</p>`
-    });
-
-    eleventyConfig.addPassthroughCopy("js");
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("/*.png");
-    eleventyConfig.addPassthroughCopy("/*.png");
-    eleventyConfig.addPassthroughCopy("/*.xml");
-    eleventyConfig.addPassthroughCopy("favicon.ico");
-    eleventyConfig.addPassthroughCopy("site.webmanifest");
-
-    eleventyConfig.addPlugin(pluginSass, {});
-    eleventyConfig.setLibrary("md", markdownIt(options));
-    return {}
-};
+  eleventyConfig.setLibrary("md", markdownIt(options))
+}
